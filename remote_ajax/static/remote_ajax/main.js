2629450c85801509c9f7e5c9b98ed7e9efc6cfd7
Petr Dlouhý <petr.dlouhy@email.cz>
2014-03-16 19:59:09 +0100

    fix data procesing in main.js

--- remote_ajax/static/remote_ajax/main.js
+++ remote_ajax/static/remote_ajax/main.js
@@ -3,17 +3,34 @@ function DjangoRemoteAjax(){
         if (data == undefined) data = "";
         if (type == undefined) type = "GET";
 
-        $.ajax({
-            headers: {'Cookie' : document.cookie },
-            url: url,
-            data: data,
-            type: type,
-            xhrFields: {withCredentials: true},
-            success: success,
-            error: function (xhr, ajaxOptions, thrownError) {
-                success("networking error: "+thrownError+'\n'+status+'\n'+xhr.statusText);
-            }
-        });
+        if( typeof FormData!='undefined'){
+           $.ajax({
+               headers: {'Cookie' : document.cookie },
+               url: url,
+               data: data,
+               type: type,
+               cache: false,
+               contentType: false,
+               processData: false,
+               xhrFields: {withCredentials: true},
+               success: success,
+               error: function (data){
+                   success(data.responseText);
+               }
+           });
+        } else {
+           $.ajax({
+               headers: {'Cookie' : document.cookie },
+               url: url,
+               data: data,
+               type: type,
+               xhrFields: {withCredentials: true},
+               success: success,
+               error: function (data){
+                   success(data.responseText);
+               }
+           });
+        };
     }
 
     this.get = function (url, success, data) {
